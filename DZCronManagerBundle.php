c1959e234833f5f1721fd300ae2135816c149194
Dsaz <dsazztazz@gmail.com>
2015-11-16 22:57:03 +0200

    Create DZCronManagerBundle.php

---
+++ DZCronManagerBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the DZCronManagerBundle.
+ *
+ * (c) Stanislav Stepanenko <dsazztazz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+namespace DZ\CronManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DZCronManagerBundle extends Bundle
+{
+}
