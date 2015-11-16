6fd97287d172dd64015ed348e150c1e7b8711a2a
Dsaz <dsazztazz@gmail.com>
2015-11-16 22:58:52 +0200

    Create Configuration.php

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DZ\CronManagerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('dz_cron_manager');
+
+        return $treeBuilder;
+    }
+}
